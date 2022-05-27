3fb68d4482c26b26bbd36300f9b4e3910a2d241e
jadson76 <biojadson@gmail.com>
2022-05-26 23:18:28 -0300

    finalizando classes de dominio

--- src/main/java/com/jadson/api/bookstore/dominio/Categoria.java
+++ src/main/java/com/jadson/api/bookstore/dominio/Categoria.java
@@ -1,17 +1,36 @@
 package com.jadson.api.bookstore.dominio;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
-@Builder
-@EqualsAndHashCode(exclude = {"nome","descricao","livros"})
-public class Categoria {
+@Entity
+public class Categoria implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/jadson/api/bookstore/dominio/Livro.java
+++ src/main/java/com/jadson/api/bookstore/dominio/Livro.java
@@ -1,18 +1,41 @@
 package com.jadson.api.bookstore.dominio;
 
 import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
-@EqualsAndHashCode(exclude = {"titulo","nomeAutor","texto","categoria"})
-public class Livro {
+@Entity
+public class Livro implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nomeAutor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return id.equals(livro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
